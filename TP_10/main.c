418873cc303d03a281388e2fbf5a3a866f4ecec1
Axoner <matigonzalez@itba.edu.ar>
2017-11-01 12:51:37 -0300

    Bienvenida y un poco del main ;)

--- TP_10/main.c
+++ TP_10/main.c
@@ -8,12 +8,28 @@
  */
 
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>      /* printf, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
+#include "Deff.h"
 
 
-int main(int argc, char** argv)
+int main(void)
 {
+    srand (time(NULL));         //inicializo mi semilla
+    
+    changemode(BUFFERED_OFF); // Turns terminal line buffering off
+    clrscr();
+    
+    bienvenida();
+    
+    while(input != EXIT || /* poner timer*/)            //salgo del while cuando el usuario lo indique y asi termina el programa
+    {
+        
+    }
+    
+    
     // ACA VA EL CODIGO ELEFANTE!!!
     
     return 0;
